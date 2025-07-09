a7ccdb0a8a9a7993718f12f5a03b37ad8c3cf750
Max Tovstopiatyi <maxrori228@gmail.com>
2025-07-09 18:45:56 +0300

    changed if esle to if

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -12,7 +12,9 @@ function calculateRentalCost(days) {
 
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
-  } else if (days < LONG_TERM && days >= SHORT_TERM) {
+  }
+
+  if (days < LONG_TERM && days >= SHORT_TERM) {
     return basePrice - SHORT_TERM_DISCOUNT;
   }
 
